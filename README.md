e0109d8221f0186ed9ae73d13e2da8da10086658
Karl Moos <karlmoos@gmail.com>
2017-09-07 13:20:55 -0500

    Init

---
+++ README.md
@@ -0,0 +1,8 @@
+# logs-a-lot Ovewview
+
+Simple python script that logs a bunch of messages to the console.  Use to test logging, etc.
+
+## Deployment on OpenShift
+
+To deploy run the following command:
+`oc new-app registry.access.redhat.com/rhscl/python-35-rhel7~https://github.com/themoosman/logs-a-lot.git`
---
+++ app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import datetime
+import logging
+import time
+import os
+import sys
+import traceback
+import random
+import string
+
+class BusyPod(object):
+
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+
+    def main(self):
+        try:
+            #formatter = json_log_formatter.JSONFormatter()
+            #log_fmt = '%(asctime)-15s %(levelname)-8s %(message)s'
+            log_fmt = '%(message)s'
+            date_fmt = '%m-%d-%Y %H:%M:%S'
+            log_level = logging.INFO
+            #logging.basicConfig(format=log_fmt, level=log_level)
+            logging.basicConfig(format=log_fmt, datefmt=date_fmt, level=log_level)
+            #logging.basicConfig(format='%(message)s', datefmt='%m-%d-%Y %H:%M:%S', level=logging.INFO)
+
+            while True:
+                #datetime.datetime.now().strftime('%m-%d-%Y %H:%M:%S'),
+                message="{ \"message\":\"%s\", \"forward_message_to_splunk\":\"%s\", \"Severity\" : \"INFO\" }" % ( ''.join(random.SystemRandom().choice(string.digits + string.ascii_letters) for _ in range(random.randint(50, 200))), str(bool(random.getrandbits(1))).lower())
+                self.logger.info(message)
+                #time.sleep(random.randint(20, 40))
+                time.sleep(random.randint(1, 10))
+
+        except KeyboardInterrupt:
+            logging.critical("Terminating due to keyboard interrupt")
+        except:
+            logging.critical("Terminating due to unexpected error: %s", sys.exc_info()[0])
+            logging.critical("%s", traceback.format_exc())
+
+if __name__ == "__main__":
+    BusyPod().main()
